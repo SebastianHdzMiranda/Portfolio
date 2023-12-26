0322ab076bf9bab4045c9143f42fdd3bf06e9f96
SebastianHdzMiranda <sebashm68@gmail.com>
2023-12-26 11:05:54 -0600

    Agregando innovacion-formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import About from './components/About';
 import Header from './components/Header';
 import Hero from './components/Hero';
+import Innovacion from './components/Innovacion';
 import Servicios from './components/Servicios'
 import './scss/app.scss';
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <Servicios />
       <About />
+      <Innovacion />
     </>
   )
 }
