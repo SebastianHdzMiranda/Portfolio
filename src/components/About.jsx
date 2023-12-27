74de89cfd41708d95b4c4ced62d8bc9c1fab3f23
SebastianHdzMiranda <sebashm68@gmail.com>
2023-12-27 15:53:10 -0600

    Modificando espacios y scroll

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,8 +7,10 @@ function About() {
         <div className="about__contenido">
             <h5 className="about__h5">About Me</h5>
             <h2 className="about__heading">Transforming visions into exceptional portfolios</h2>
-            <p className="about__texto">Nemo design enim ipsam voluptatem quim voluptas sit aspernatur aut odit auting fugit sed thisnquia consequuntur magni dolores eos designer heresm qui ratione</p>
-            <a href="#" className="about__enlace">Descargar CV</a>
+            <div className="about__info">
+              <p className="about__texto">Nemo design enim ipsam voluptatem quim voluptas sit aspernatur aut odit auting fugit sed thisnquia consequuntur magni dolores eos designer heresm qui ratione</p>
+              <a href="#" className="about__enlace">Descargar CV</a>
+            </div>
         </div>
 
         <div className="about__grafico">
