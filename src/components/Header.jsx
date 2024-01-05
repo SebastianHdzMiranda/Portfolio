9e9720a00f90de2c12658c13bf2133d6cecfdd79
SebastianHdzMiranda <sebashm68@gmail.com>
2024-01-05 09:16:02 -0600

    Refactorizando codigo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,32 +3,55 @@ import logo from '../assets/logo2.svg'
 import hamburguer from '../assets/menu.svg';
 
 function Header() {
+
+  function mostrarNav(e) {
+    e.preventDefault();
+    // variables
+    const nav = document.querySelector('.navegacion');
+    const menu  = document.querySelector('#menu');
+    const body = document.querySelector('body')
+
+
+    // condiciones
+    if (nav.classList.contains('navegacion--activo')) {
+        nav.classList.remove('navegacion--activo');
+        menu.classList.remove('is-active');
+        body.style.overflowY = 'auto';
+    } else{
+        nav.classList.add('navegacion--activo');
+        menu.classList.add('is-active');
+        body.style.overflowY = 'hidden';
+    }
+  }
   return (
     <header className='header contenedor'>
-        <div className="header__logo-content">
-          <div className="header__logo">
-            <a href='/'>
-              <img className='header__img' src={logo} alt="Logo"/>
+        <div className="header__contenido">
+
+          <div className="header__logo-content">
+            <div className="header__logo">
+              <a href='/'>
+                <img className='header__img' src={logo} alt="Logo"/>
+              </a>
+            </div>
+          </div>
+
+          <nav className='navegacion'>
+            <a href="#" className="navegacion__enlace">Home</a>
+            <a href="#" className="navegacion__enlace">About</a>
+            <a href="#" className="navegacion__enlace">Service</a>
+            <a href="#" className="navegacion__enlace">Projects</a>
+            <a href="#" className="navegacion__enlace">Contact</a>
+          </nav>
+
+          <div className="header__hamburger">
+            {/* <img className="header__hamburgerImg" src={hamburguer} alt="" /> */}
+
+            <a className="header__iconMenu hamburger hamburger--slider" type="button" id="menu" onClick={mostrarNav}>
+              <span className="hamburger-box">
+              <span className="hamburger-inner"></span>
+              </span>
             </a>
           </div>
-        </div>
-
-        <nav className='navegacion'>
-          <a href="#" className="navegacion__enlace">Home</a>
-          <a href="#" className="navegacion__enlace">About</a>
-          <a href="#" className="navegacion__enlace">Service</a>
-          <a href="#" className="navegacion__enlace">Projects</a>
-          <a href="#" className="navegacion__enlace">Contact</a>
-        </nav>
-
-        <div className="header__hamburger">
-          {/* <img className="header__hamburgerImg" src={hamburguer} alt="" /> */}
-
-          <a className="header__iconMenu hamburger hamburger--slider" type="button" id="menu">
-            <span className="hamburger-box">
-            <span className="hamburger-inner"></span>
-            </span>
-          </a>
         </div>
     </header>
   )
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ function Hero() {
         <div className="hero__contenedor">
             <div className="hero__contenido">
                 <h1 className="hero__heading">Hola, Soy Sebastian! <span className='hero__heading-span'>Front-End</span> Developer</h1>
-                <p className="hero__descripcion">Soy un apasionado diseñador UI/UX con la misión de crear experiencias digitales encantadoras e intuitivas. Con una base sólida en principios de diseño y un gran ojo para los detalles, me especializo en traducir ideas complejas en interfaces fáciles de usar que cautivan y atraen.</p>
+                <p className="hero__descripcion">Soy un apasionado desarrollador web con la misión de crear experiencias digitales encantadoras e intuitivas. Con una base sólida en principios de front-end, me especializo en traducir ideas complejas en interfaces fáciles de usar que cautivan y atraen.</p>
                 <a href="#" className="hero__boton">Descargar CV</a>
             </div>
 
